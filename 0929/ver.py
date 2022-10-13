957b25513ede5eebd945827c4f275dd0f9b5bef5
ogi-y <ioshelg@gmail.com>
2022-10-13 22:45:57 +0900

    programs

---
+++ 0929/ver.py
@@ -0,0 +1,4 @@
+name = "私" #文字列に名前を付ける
+money = 500 #数字に名前を付ける
+print(name + "の所持金: " + str(money) + "円") #文字列をくっつける
+print("{}の所持金: {}円".format(name,money)) #別の書き方
---
+++ 1006/plus_b_stdin.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import sys
+
+ans[float(e) for e in sys.stdin]
+print(sum(ans))
