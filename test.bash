a8b0b27b83bfca7eb43d397401b4d69bda1512e3
ogishima <ioshelg@gmail.com>
2022-11-03 11:39:51 +0900

    add

---
+++ test.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Yoshihiro Ogishima
+# SPDX-Licende-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK!
+exit $res
